1f7f7d820d5392b0dfd8122a0872f1d31eb48c7f
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2022-01-11 21:41:31 -0300

    feat: Component button | hook authenticate and initial config firebase

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import AppLoading from 'expo-app-loading';
 import { StatusBar } from 'expo-status-bar';
 import { useFonts, DMSans_400Regular } from '@expo-google-fonts/dm-sans';
 import { DMSerifDisplay_400Regular } from '@expo-google-fonts/dm-serif-display';
 import { ThemeProvider } from 'styled-components/native';
+import { AuthProvider } from '@hooks/auth';
 
-import theme from '@src/theme';
-import { SignIn } from '@src/screens/SignIn';
+import theme from './src/theme';
+import { SignIn } from '@screens/SignIn';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
@@ -21,7 +23,10 @@ export default function App() {
   return (
     <ThemeProvider theme={theme}>
       <StatusBar style='light' translucent backgroundColor="transparent" />
-      <SignIn />
+
+      <AuthProvider>
+        <SignIn />
+      </AuthProvider>
     </ThemeProvider>
   );
 }
--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
+import { TextInputProps } from 'react-native';
 
 import {
   Container,
+  TypeProps,
 } from './styles';
 
-export function Input() {
-  return (
-    <Container>
+type Props = TextInputProps & {
+  type?: TypeProps;
+}
 
-    </Container>
+export function Input({ type = 'primary', ...rest }: Props) {
+  return (
+    <Container type={type} {...rest} />
   );
 }
