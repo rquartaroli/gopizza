748bf80b436044e713d46537642f7f17afba76e2
Rafael Quartaroli <rafaelquartaroli@hotmail.com>
2022-01-12 21:09:28 -0300

    feat: New Page Home and new component Search

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { MaterialIcons } from '@expo/vector-icons';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from 'styled-components/native';
+
+import happyEmoji from '@assets/happy.png';
+
+import { Search } from '@components/Search';
+
+import {
+  Container,
+  Header,
+  Greeting,
+  GreetingEmoji,
+  GreetingText,
+  Title,
+  MenuHeader,
+  MenuItemsNumber,
+} from './styles';
+
+export function Home() {
+  const { COLORS } = useTheme();
+
+  return (
+    <Container>
+      <Header>
+        <Greeting>
+          <GreetingEmoji source={happyEmoji} />
+          <GreetingText>Olá, Admin</GreetingText>
+        </Greeting>
+
+        <TouchableOpacity>
+          <MaterialIcons
+            name="logout"
+            color={COLORS.TITLE}
+            size={24}
+          />
+        </TouchableOpacity>
+
+      </Header>
+
+      <Search onSearch={() => { }} onClear={() => { }} />
+
+      <MenuHeader>
+        <Title>Cardápio</Title>
+        <MenuItemsNumber>10 pizzas</MenuItemsNumber>
+      </MenuHeader>
+    </Container>
+  );
+}
--- src/screens/Product/index.tsx
+++ src/screens/Product/index.tsx
@@ -1,9 +1,13 @@
 import React, { useState } from 'react';
-import { Platform, TouchableOpacity } from 'react-native';
+import { Platform, TouchableOpacity, ScrollView, Alert } from 'react-native';
 import * as ImagePicker from 'expo-image-picker';
+import firestore from '@react-native-firebase/firestore';
+import storage from '@react-native-firebase/storage';
 
 import { ButtonBack } from '@components/ButtonBack';
 import { InputPrice } from '@components/InputPrice';
+import { Button } from '@components/Button';
+import { Input } from '@components/Input';
 import { Photo } from '@components/Photo';
 
 import {
@@ -13,10 +17,21 @@ import {
   DeleteLabel,
   Upload,
   PickImageButton,
+  Form,
+  Label,
+  InputGroup,
+  InputGroupHeader,
+  MaxCharacters,
 } from './styles';
 
 export function Product() {
   const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [description, setDescription] = useState('');
+  const [priceSizeP, setPriceSizeP] = useState('');
+  const [priceSizeM, setPriceSizeM] = useState('');
+  const [priceSizeG, setPriceSizeG] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function handlePickerImage() {
     const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
@@ -33,31 +48,125 @@ export function Product() {
     }
   }
 
+  async function handleAdd() {
+    if(!name.trim()) {
+      return Alert.alert('Cadastro', 'Informe o nome da pizza.');
+    }
+
+    if(!description.trim()) {
+      return Alert.alert('Cadastro', 'Informe a descrição da pizza.');
+    }
+
+    if(!image) {
+      return Alert.alert('Cadastro', 'Selecione a imagem da pizza.');
+    }
+
+    if(!priceSizeP || !priceSizeM || !priceSizeG) {
+      return Alert.alert('Cadastro', 'Selecione o preço de todos os tamanhos da pizza.');
+    }
+
+    setIsLoading(true);
+
+    const fileName = new Date().getTime();
+    const reference = storage().ref(`/pizzas/${fileName}.png`);
+
+    await reference.putFile(image);
+    const photo_url = await reference.getDownloadURL();
+
+    firestore()
+    .collection('pizzas')
+    .add({
+      name,
+      name_insensitive: name.toLowerCase().trim(),
+      description,
+      prices_sizes: {
+        p: priceSizeP,
+        m: priceSizeM,
+        g: priceSizeG,
+      },
+      photo_url,
+      photo_path: reference.fullPath
+    })
+    .then(() => Alert.alert('Cadastro', 'Pizza cadastrada com sucesso.'))
+    .catch(() => Alert.alert('Cadastro', 'Não foi possível cadastrar a pizza.'))
+
+    setIsLoading(false);
+  }
+
   return (
     <Container behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
-      <Header>
-        <ButtonBack />
+      <ScrollView showsVerticalScrollIndicator={false}>
+        <Header>
+          <ButtonBack />
 
-        <Title>Cadastrar</Title>
-        
-        <TouchableOpacity>
-          <DeleteLabel>Deletar</DeleteLabel>
-        </TouchableOpacity>
-      </Header>
+          <Title>Cadastrar</Title>
+          
+          <TouchableOpacity>
+            <DeleteLabel>Deletar</DeleteLabel>
+          </TouchableOpacity>
+        </Header>
 
-      <Upload>
-        <Photo uri={image} />
-        
-        <PickImageButton 
-          title='Carregar' 
-          type='secondary'
-          onPress={handlePickerImage}
-        />
-      </Upload>
+        <Upload>
+          <Photo uri={image} />
+          
+          <PickImageButton 
+            title='Carregar' 
+            type='secondary'
+            onPress={handlePickerImage}
+          />
+        </Upload>
 
-      <InputPrice size="P" />
-      <InputPrice size="M" />
-      <InputPrice size="G" />
+        <Form>
+          <InputGroup>
+            <Label>Nome</Label>
+            <Input 
+              onChangeText={setName} 
+              value={name} 
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <InputGroupHeader>
+              <Label>Descrição</Label>
+              <MaxCharacters>0 de 60 caracteres</MaxCharacters>
+            </InputGroupHeader>
+
+            <Input 
+              multiline
+              maxLength={60}
+              style={{ height: 80 }}
+              onChangeText={setDescription} 
+              value={description}
+            />
+          </InputGroup>
+
+          <InputGroup>
+            <Label>Tamanhos e preços</Label>
+            <InputPrice 
+              size="P" 
+              onChangeText={setPriceSizeP} 
+              value={priceSizeP}
+            />
+            <InputPrice 
+              size="M" 
+              onChangeText={setPriceSizeM} 
+              value={priceSizeM}
+            />
+            <InputPrice 
+              size="G" 
+              onChangeText={setPriceSizeG} 
+              value={priceSizeG}
+            />
+          </InputGroup>
+
+          <Button 
+            title="Cadastrar pizza" 
+            isLoading={isLoading}
+            onPress={handleAdd}
+          />
+
+        </Form>
+      </ScrollView>
     </Container>
   );
 }
